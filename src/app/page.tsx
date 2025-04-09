304fcb567bd9d8a267a8555303cc874231749ada
riccc787 <rrbarrancob@gmail.com>
2025-04-09 08:59:47 -0500

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ChatContainer from '@/components/chat/ChatContainer';
-
-export default function Home() {
-  return (
-    <main className="min-h-screen">
-      <ChatContainer />
-    </main>
-  );
-}
